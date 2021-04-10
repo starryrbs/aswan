48699b00da2b364a2e2da70007c86c0a3dd426de
starryrbs <1322096624@qq.com>
2021-04-10 12:13:10 +0800

    配置mysql密码

--- www/settings/local_settings/develop.py
+++ www/settings/local_settings/develop.py
@@ -5,10 +5,7 @@ import os
 DATABASES = {
     'default': {
         "ENGINE": 'django.db.backends.mysql',
-        "HOST": "127.0.0.1",
-        "PORT": 3306,
         "USER": "root",
-        "PASSWORD": os.environ.get("MYSQL_PASSWORD", "root"),
         "DATABASE_CHARSET": "utf8",
         "NAME": "risk_control",
     },
